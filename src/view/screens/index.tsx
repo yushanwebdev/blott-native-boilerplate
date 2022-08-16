28f5d1a8942d678a65f3ab3968aae325ffcd88ab
yushan <yushanwebdev@gmail.com>
2022-08-16 14:55:46 +0530

    feat: Remove NativeBase

--- src/view/screens/index.tsx
+++ src/view/screens/index.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
-import {Provider} from 'react-redux';
 import {Navigation} from 'react-native-navigation';
+import {Provider} from 'react-redux';
 
 import {SCREENS} from '../../constants/screen';
 
-import * as Splash from './splash';
-import * as Home from './home';
-import * as Settings from './settings';
 import * as Drawer from './drawer';
 import * as Dummy from './dummy';
-import {NativeBaseProvider} from 'native-base';
+import * as Home from './home';
+import * as Settings from './settings';
+import * as Splash from './splash';
 
 const registerComponentWithRedux =
   (redux: any) => (name: string, screen: any) => {
@@ -17,11 +16,9 @@ const registerComponentWithRedux =
       name,
       () => (props: any) =>
         (
-          <NativeBaseProvider>
-            <Provider store={redux.store}>
-              <screen.default {...props} />
-            </Provider>
-          </NativeBaseProvider>
+          <Provider store={redux.store}>
+            <screen.default {...props} />
+          </Provider>
         ),
       () => screen.default,
     );
