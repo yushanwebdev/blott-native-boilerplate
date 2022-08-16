96e73a707dbd473451e38d783610e794648cba67
yushan <yushanwebdev@gmail.com>
2022-08-16 15:57:02 +0530

    feat: Settings Page Issue - Fix an loading issue in initial mount

--- src/view/screens/settings/index.tsx
+++ src/view/screens/settings/index.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
-import {Text} from 'react-native';
+import {ScrollView, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {GLOBAL} from '../../styles/global';
 
 export interface Props {}
 
 const Settings: React.FC<Props> = () => {
-  return <Text>Settings</Text>;
+  return (
+    <SafeAreaView style={GLOBAL.LAYOUT.SafeArea}>
+      <ScrollView style={GLOBAL.LAYOUT.pageContainer}>
+        <Text>Settings</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default Settings;
